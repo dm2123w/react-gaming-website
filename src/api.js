bc38bff7a343e8ea653fc2636b1a62c761cf9756
dmitriysemenko <dm123semenko@gmail.com>
2021-09-01 21:42:34 +0500

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
-import key from "../key";
-
 // Base URL
-const base_url = "http://api.rawg.io/api";
+const base_url = "http://api.rawg.io/api/";
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
 
 // Getting the date
 const getCurrentMonth = () => {
@@ -16,8 +15,23 @@ const getCurrentMonth = () => {
 const getCurrentDay = () => {
     const day = new Date().getDate();
     if (day < 10) {
-        return `0${month}`;
+        return `0${day}`;
     } else {
         return day;
     }
 };
+
+// Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nexYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// Popular Games
+const popular_games = `games?key=${REACT_APP_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+const popularGamesURL = () => `${base_url}${popular_games}`;
+
+console.log(popularGamesURL());
